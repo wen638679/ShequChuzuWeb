4ea79f4a095bdec5d019d4c9ec0fd9b2d1804dfa
wenzhaowen <13763394340@163.com>
2017-08-25 00:08:37 +0800

    注释 module/lease

--- app/module/lease.js
+++ app/module/lease.js
@@ -5,24 +5,24 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 const leaseSchema = new Schema({
-    userId:{type:Schema.Types.ObjectId,require:true},
-    locationId:{type:Schema.Types.ObjectId,require:true},
-    title:{type:String},
-    tags:[String],
-    message:String,
-    picture:[String],
-    deposit:{type:Number,require:true},
-    rent:{type:Number,require:true},
-    rentCycle:{type:Number,require:true,default:1},
-    conditions:[String],
-    status:{type:String,enum:[0,1,2,3,4,5,6],require:true},
-    likesUser:[Schema.Types.ObjectId],
-    leaseUser:Schema.Types.ObjectId,
-    createDate:Date,
-    publishDate:Date,
-    changeDate:Date,
-    leaseDate:Date,
-    revertDate:Date,
+    userId:{type:Schema.Types.ObjectId,require:true}, //发表用户ID
+    locationId:{type:Schema.Types.ObjectId,require:true}, //位置ID
+    title:{type:String}, //标题
+    tags:[String], //标签
+    message:String, //信息
+    picture:[String], //图片
+    deposit:{type:Number,require:true}, //押金
+    rent:{type:Number,require:true}, //租金
+    rentCycle:{type:Number,require:true,default:1}, //周期/天
+    conditions:[String], //要求
+    status:{type:String,enum:[0,1,2,3,4,5,6],require:true}, //状态
+    likesUser:[Schema.Types.ObjectId], //感兴趣的用户ID
+    leaseUser:Schema.Types.ObjectId, //租客用户ID
+    createDate:Date, //创建时间
+    publishDate:Date, //发布时间
+    changeDate:Date, //修改时间
+    leaseDate:Date, //出租时间
+    revertDate:Date, //归还时间
 });
 const Lease = mongoose.model('Lease',leaseSchema);
 export default Lease;
